5d1319522c1c1f3fbd1cd92725585d348efd44ab
Rafael <aguilera.morales.rafa@gmail.com>
2022-06-21 14:04:16 -0700

    initial Commit

--- public/main.js
+++ public/main.js
@@ -2,56 +2,43 @@
 
 let btn = document.getElementById("calculate-change");
 
-
 function handleClickEvent() {
-
         let quartercount = 0
         let dimescount = 0
         let nickelcount = 0
         let penniescount = 0
         let amountDue = document.getElementById("amount-due").value;
         let amountTendered = document.getElementById("amount-received").value;
-        console.log(amountDue);
-        console.log(amountTendered);
+            console.log(amountDue);
+            console.log(amountTendered);
         let change = amountTendered - amountDue;
             console.log(change);
         
-     if (change >= 0) {
+    if (change >= 0) {
         let Dollars = Math.floor(change);
-        console.log(Dollars);
-
+            console.log(Dollars);
         let pocketChange = change-Dollars;
-       
+
         quartercount = Math.floor(pocketChange/.25); 
         quarterremainder = pocketChange%.25; 
-
         dimescount = Math.floor( quarterremainder/.10); 
         dimesremainder = quarterremainder%.10; 
-
         nickelcount = Math.floor(dimesremainder/.05); 
         nickelremainder = dimesremainder%.05; 
-
         penniescount = Math.round( nickelremainder/.01); 
-        
-        console.log(quartercount)
-        console.log(dimescount)
-        console.log(nickelcount)
-        console.log(penniescount)
-
+            console.log(quartercount)
+            console.log(dimescount)
+            console.log(nickelcount)
+            console.log(penniescount)
         document.getElementById("change").textContent = `Your total change is: $ ${change.toFixed(2)}dlls.`
-
         document.getElementById("dollars-output").textContent = Dollars
-        
         document.getElementById("quarters-output").textContent = quartercount
-
         document.getElementById("dimes-output").textContent = dimescount
-
         document.getElementById("nickels-output").textContent = nickelcount
-
         document.getElementById("pennies-output").textContent = penniescount
             }
 
-        else
+    else
         document.getElementById("no-money").textContent = `YOU NEED MORE CASH BRO!!!.`
 
 } ;
